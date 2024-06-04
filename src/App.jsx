e954ac1ffe1708c19f437e8e3a82f4a796e8d0ac
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2024-06-04 16:16:01 +0300

    dist deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <PageContainer>
-      <Router basename='/E-Commerce-ReduxToolkit'>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
